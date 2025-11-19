6b844a601e0ebaa3417173e0e11f205e62272137
evan5622 <bruchecevan@gmail.com>
2025-11-19 11:06:10 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "gauche") {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+radio.setGroup(1)
 basic.forever(function () {
-	
+    basic.clearScreen()
+    basic.pause(1000)
 })
